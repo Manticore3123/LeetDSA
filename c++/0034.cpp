120eeb77a71be018d3e63609890c88e22df6952a
first repo <saideep.acrgk22@sinhgad.edu>
2025-06-28 13:32:59 +0530

    adding solution to leetcode 33

---
+++ c++/0034.cpp
@@ -0,0 +1,56 @@
+class Solution {
+private:
+        int startPosition(vector<int>& nums,int target){
+        int st = 0 ;
+        int end = nums.size()-1;
+        int ans = -1;
+
+        while(st<=end){
+            int mid = st + (end - st)/2;
+
+            if(target<nums[mid]){
+                end = mid - 1;
+            }
+            if(target>nums[mid]){
+                st = mid + 1;
+            }
+            else if(target==nums[mid]){
+                    ans = mid;
+                    end = mid - 1;  
+            }
+        }
+        return ans;
+        }
+
+private:
+        int lastPosition(vector<int>& nums,int target){
+        int st = 0 ;
+        int end = nums.size()-1;
+        int ans = -1;
+
+        while(st<=end){
+            int mid = st + (end - st)/2;
+
+            if(target<nums[mid]){
+                end = mid - 1;
+            }
+            if(target>nums[mid]){
+                st = mid + 1;
+            }
+            
+                if(nums[mid]==target){
+                    ans = mid;
+                    st = mid + 1;
+                }
+        }
+        return ans;
+        }
+public:
+        vector<int> searchRange(vector<int>& nums,int target){
+            int start = startPosition(nums,target);
+            int end = lastPosition(nums,target);
+
+            return {start,end};
+        }
+        
+};
